7d4dc09f2baffbbc8a3b7592c135cfe2048715f4
VenkataHemanth16320 <65711770+VenkataHemanth16320@users.noreply.github.com>
2024-01-04 22:18:29 -0500

    Update provider.tf

--- Terraform Scripts/Ansible_Server/provider.tf
+++ Terraform Scripts/Ansible_Server/provider.tf
@@ -8,13 +8,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "project-register"
+    bucket = "project-register16320"
     key    = "ansible/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
 
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
